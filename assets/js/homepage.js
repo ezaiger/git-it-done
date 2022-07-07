b91784327a6b21345f0faf8e948efc9b19e46e0e
EZaiger <elisabeth.zaiger@oit.edu>
2022-07-06 21:51:00 -0700

    adding complex queries

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 // form submission
 var formSubmitHandler = function (event) {
@@ -17,6 +18,15 @@ var formSubmitHandler = function (event) {
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
+    }
+};
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language")
+    if (language) {
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent = "";
     }
 };
 
@@ -40,6 +50,20 @@ var getUserRepos = function (user) {
             // notice this '.catch(); getting chained  onto the end of the '.then()' method
             alert("Unable to connect to GitHub");
         });
+};
+
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
 };
 
 // display repository & user information
@@ -80,7 +104,7 @@ var displayRepos = function (repos, searchTerm) {
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
-
 };
 
-userFormEl.addEventListener("submit", formSubmitHandler)
+userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
